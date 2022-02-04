ed6ea216d1388499a3f56d926d70fdbd0afa9cf2
Jertrude Fides Olaer Seguisa <jertrude.fides@gmail.com>
2022-02-04 10:35:10 +0800

    Updated instruction_decoder
    Signed-off-by: Jertrude Fides Olaer Seguisa <jseguisa@pve-vm-102.home.net>

--- design/instruction_decoder.sv
+++ design/instruction_decoder.sv
@@ -1,52 +1,45 @@
 module instruction_decoder (
-        input i_bus,
-        output lda,
-        output add,
-        output sub,
-        output out,
-        output hlt_bar);
-    
-    logic [7:4] i_bus;
-    logic lda;
-    logic add;
-    logic sub;
-    logic out;
-    logic hlt_bar;
+        input logic [7:4]   i_bus,
+        output logic        lda,
+        output logic        add,
+        output logic        sub,
+        output logic        out,
+        output logic        hlt_bar);
     
     always_comb begin
         if (i_bus == 'h0) begin
-            lda <= 1'b1;
+            lda = 1'b1;
         end
         else begin
-            lda <= 1'b0;
+            lda = 1'b0;
         end
         
         if (i_bus == 'h1) begin
-            add <= 1'b1;
+            add = 1'b1;
         end
         else begin
-            add <= 1'b0;
+            add = 1'b0;
         end
         
         if (i_bus == 'h2) begin
-            sub <= 1'b1;
+            sub = 1'b1;
         end
         else begin
-            sub <= 1'b0;
+            sub = 1'b0;
         end
         
         if (i_bus == 'hE) begin
-            out <= 1'b1;
+            out = 1'b1;
         end
         else begin
-            out <= 1'b0;
+            out = 1'b0;
         end
         
         if (i_bus == 'hF) begin
-            hlt_bar <= 1'b0;
+            hlt_bar = 1'b0;
         end
         else begin
-            hlt_bar <= 1'b1;
+            hlt_bar = 1'b1;
         end
     end
 endmodule
